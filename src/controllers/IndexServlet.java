1749d96875b9c35d342c0b367023cf85bdea4bf1
itosu.asaya <tss.23781@gamail.com>
2023-07-07 17:12:40 +0900

    SELECT文追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,23 +1,34 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Tasks;
+import utils.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public IndexServlet() {
-        super();
-    }
+	public IndexServlet() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		List<Tasks> tasks = em.createNamedQuery("getAlltasks", Tasks.class).getResultList();
+		response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+		em.close();
 	}
 
 }
